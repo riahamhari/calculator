cffa9d1326e767bc3cbedfb78d6d303ecff59781
riahamhari <109351021+riahamhari@users.noreply.github.com>
2022-10-07 15:43:50 +0100

    event listeners for buttons and math operator functions

--- app.js
+++ app.js
@@ -1,35 +1,137 @@
+let shouldResetScreen = false;
+let currentOperation = null
+let firstNum = ''
+let secondNum = ''
+
+
 // Number Keys
-const number = document.querySelectorAll('.numBtn')
+const numbers = document.querySelectorAll('.numBtn')
 
 // Clear Keys
 const clearBtn = document.querySelector('#clear');
 const deleteBtn = document.querySelector('#delete');
 
+// operators
+const operators = document.querySelectorAll('.opBtn')
 
-const display = document.querySelector('.display')
-const calculation = document.querySelector('.calculation')
+const equalsBtn = document.querySelector('#equals')
+
+const currentSel = document.querySelector('#currentSel')
+const lastSel = document.querySelector('#lastSel')
+
+// event listeners
+
+numbers.forEach((button) =>
+    button.addEventListener('click', () => appendNumber(button.textContent))
+)
+
+
+operators.forEach((button) =>
+    button.addEventListener('click', () => setOperation(button.textContent))
+)
+equalsBtn.addEventListener('click', evaluate)
+clearBtn.addEventListener('click', clear)
+deleteBtn.addEventListener('click', deleteNum)
+
+
+// functions
+
+
+function appendNumber(number) {
+    if (currentSel.textContent === '0' || shouldResetScreen)
+        resetScreen()
+    currentSel.textContent += number;
+
+}
+
+function resetScreen() {
+    currentSel.textContent = '';
+    shouldResetScreen = false;
+
+}
+
+function clear() {
+    currentSel.textContent = '0'
+    lastSel.textContent = ''
+    firstNum = ''
+    secondNum = ''
+    currentOperation = null
+}
+
+function deleteNum() {
+    currentSel.textContent = currentSel.textContent
+        .toString()
+        .slice(0, -1)
+}
+
+function setOperation(operator) {
+    if (currentOperation !== null) evaluate()
+    firstNum = currentSel.textContent
+    currentOperation = operator
+    lastSel.textContent = `${firstNum}${operator}`
+
+    shouldResetScreen = true
+
+}
+
+function evaluate() {
+    if (currentOperation === null || shouldResetScreen) return
+    if (currentOperation === '÷' && currentSel.textContent === '0') {
+        alert("You can't divide by 0!")
+        return
+    }
+    secondNum = currentSel.textContent
+    currentSel.textContent = roundResult(
+        operate(currentOperation, firstNum, secondNum)
+    )
+
+    lastSel.textContent = `${firstNum}${currentOperation}${secondNum}=`
+    currentOperation = null
+}
+
+
+function roundResult(number) {
+    return Math.round(number * 1000) / 1000
+}
+
+
+function add(a, b) {
+    return a + b
+}
+
+function substract(a, b) {
+    return a - b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+    return a / b
+}
 
 
 
-
-function operate(operator, x, y) {
+function operate(operator, a, b) {
+    a = Number(a)
+    b = Number(b)
     switch (operator) {
-        case '/':
-            answer = x / y
-            break
         case '+':
-            answer = x + y
-            break
+            return add(a, b)
         case '-':
-            answer = x - y
-            break
-
-        case '*':
-            answer = x * y
-            break
-
+            return substract(a, b)
+        case 'x':
+            return multiply(a, b)
+        case '÷':
+            if (b === 0) return null
+            else return divide(a, b)
+        default:
+            return null
     }
 }
+
+
 
 
 
